d7a166a9a448882559cd41c67dd4071dde6cfdb1
Kangaroobits <kangaroobits2017@gmail.com>
2017-10-04 20:19:24 +0200

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define KangarooBits2-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "KangarooBits2URI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
